3fb8c53ba8e1728a6f3227093d9cc4a5efaab81c
abhishek sajwan <abhisheksajwan2004@gmail.com>
2025-07-23 18:13:51 +0530

    login route done

--- nextjs/src/app/api/users/login/route.ts
+++ nextjs/src/app/api/users/login/route.ts
@@ -1,15 +1,48 @@
 import { connect } from "@/dbConfig/dbConfig";
 import { User } from "@/models/userModel";
-
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { NextRequest, NextResponse } from "next/server";
 connect();
 
-export async function POST(request:NextRequest0{
-    try{
-        const reqBody = await request.json();
-        const {emailId,password} = reqBody
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { emailId, password } = reqBody;
+    const user = await User.findOne(emailId);
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: "user doesn't exist",
+        },
+        { status: 400 }
+      );
     }
-    catch(err){
-
+    console.log("user exists");
+    const validPass = await bcryptjs.compare(password, user.password);
+    if (!validPass) {
+      return NextResponse.json(
+        {
+          error: "Check your credentials",
+        },
+        { status: 400 }
+      );
     }
-})
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      emailId: user.emailId,
+    };
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1hr",
+    });
+    const response = NextResponse.json({
+      message: "Logged In Success",
+      success: true,
+    });
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+    return response;
+  } catch (err) {}
+}
---
+++ nextjs/src/app/api/users/logout/route.ts
@@ -0,0 +1,11 @@
+import { connect } from "@/dbConfig/dbConfig";
+import { User } from "@/models/userModel";
+import bcryptjs from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+   Next
+  } catch (err) {}
+}
